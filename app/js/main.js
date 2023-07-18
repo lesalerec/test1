2b4eee910c55640ecf4df6c0f95b46eab37da20f
Yavorsky <lesalerec@gmail.com>
2023-07-18 21:36:15 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,109 @@
+// ! ============= ---001--- ================
+// 'use strict';
+//
+// var leftBorderRight = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(leftBorderRight);
+// ! ========================================
+// ! ============= ---002--- ================
+// 'use strict';
+//
+// console.log(4 / 0);
+// console.log('string' * 9);
+// let something;
+// console.log(something);
+//
+// let person = {
+// 	name: 'John',
+// 	age: 25,
+// 	isMarried: false
+// };
+// console.log(person.name);
+// console.log(person['age']);
+//
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+// ! ========================================
+// ! ============= ---003--- ================
+// 'use strict';
+//
+// const obj = {
+// 	name: 'Alex',
+// 	age: '30'
+// };
+//
+// //            0    1    2    3
+// const arr = ['a', 'b', 'c', 'd'];
+//
+// // to String
+// // 1)
+// console.log(typeof(String(5)));
+// // 2)
+// console.log(typeof('https://vk.com/catalog/' + 5));
+// // to Number
+// // 1)
+// console.log(typeof(Number('5')));
+// // 2)
+// console.log(typeof(+'5'));
+// // 3)
+// console.log(typeof(parseInt('15px', 10)));
+//
+// let ans = +prompt('Hello?', '');
+// console.log('ans');
+//
+// // false === 0, '', null, undefined, NaN;
+// // 1)
+// let switcher = 0;
+// if (switcher) {
+// 	console.log('Working...');
+// }
+//
+// switcher = 1;
+// if (switcher) {
+// 	console.log('Working...');
+// }
+// // 2)
+// console.log(typeof(Boolean('5')));
+// // 3)
+// console.log(typeof(!!('5')));
+// ! ========================================
+// ! ============= ---004--- ================
+// 'use strict';
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// // alert('Hello world!');
+// // let answer = confirm('Are you here?');
+// // console.log(answer);
+// let answer = prompt('Are you 18 years old?', '');
+// console.log(typeof(answer));
+// ! ========================================
+// ! ============= ---005--- ================
+// 'use strict';
+
+// let answer = +prompt('Are you 18 years old?', '');
+// console.log(typeof(answer));
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+
+// let incr = 10,
+// 		decr = 10;
+// incr++;
+// decr--;
+
+// console.log(--incr);
+// console.log(++decr);
+//
+// console.log(5 % 2);
+// console.log('2' === 2);
+//
+// let isChecked = false;
+// let isClose = true;
+// console.log(!isChecked || isClose);
+// ! ========================================
+// ! ============= ---006--- ================
+'use strict';
+
+
+
+// ! ========================================
